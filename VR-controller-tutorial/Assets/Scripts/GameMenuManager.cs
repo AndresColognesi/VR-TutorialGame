a9cb622168514dbb5b27284dfc28c19054d8e65d
AndresColognesi <a.cologne9@gmail.com>
2023-01-02 17:52:39 -0300

    fix: Remove menu offset value from interface

--- VR-controller-tutorial/Assets/Scripts/GameMenuManager.cs
+++ VR-controller-tutorial/Assets/Scripts/GameMenuManager.cs
@@ -16,7 +16,7 @@ public class GameMenuManager : MonoBehaviour
     // Reference to player head:
     [SerializeField] private Transform head;
     // Offset distance from menu to head:
-    [SerializeField] float spawnDistance = 0.5f;
+    private float spawnDistance = 1f;
 
 
 
