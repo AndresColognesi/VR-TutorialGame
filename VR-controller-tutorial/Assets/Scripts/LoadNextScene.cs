6ce918a64508a3135b09a79ec08eb221dd2d43d1
AndresColognesi <a.cologne9@gmail.com>
2023-01-05 12:43:00 -0300

    fix: Remove default main camera from VR Tutorial scene

--- VR-controller-tutorial/Assets/Scripts/LoadNextScene.cs
+++ VR-controller-tutorial/Assets/Scripts/LoadNextScene.cs
@@ -9,7 +9,6 @@ public class LoadNextScene : MonoBehaviour
 
     public void LoadNext()
     {
-        Debug.Log("The function is being triggered and the current buid index is" + SceneManager.GetActiveScene().buildIndex.ToString());
         // Load the next scene based on the build index number:
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
