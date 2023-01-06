d4d29117eee8fb9249566bfdd104b90afec328dd
AndresColognesi <a.cologne9@gmail.com>
2023-01-06 12:09:58 -0300

    feat: Add system to load next scene when level is complete

--- VR-controller-tutorial/Assets/Scripts/ScoreCounter.cs
+++ VR-controller-tutorial/Assets/Scripts/ScoreCounter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScoreCounter : MonoBehaviour
 {
@@ -8,8 +9,29 @@ public class ScoreCounter : MonoBehaviour
     // Properties //
     //------------//
 
-    // Total score for the given level:
+    // Current total score for the given level:
     private static int total_score = 0;
+    // Desired score to complete the level:
+    private int finish_level_score;
+
+    // To store load next scene script of the Game Manager:
+    private LoadNextScene loadNextScene;
+
+
+    //------------------------//
+    //--- Built-in Methods ---//
+    //------------------------//
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set the finishing level score for the current scene:
+        finish_level_score = SceneManager.GetActiveScene().buildIndex * 10;
+
+        // Get the LoadNextLevel component of this game object:
+        loadNextScene = gameObject.GetComponent<LoadNextScene>();
+    }
+
 
 
     //---------------//
@@ -42,5 +64,21 @@ public class ScoreCounter : MonoBehaviour
          ***/
 
         total_score = 0;
+    }
+
+    public void CheckLevelFinished()
+    {
+        /***
+         * Based on the scene index, checks if score has reached
+         * the desired value for the level and changes to next scene/level.
+         ***/
+
+        if (total_score == finish_level_score) //meaning the level is complete
+        {
+            // Reset the total score:
+            ClearTotalScore();
+            // Change to next level:
+            loadNextScene.LoadNext();
+        }
     }
 }
